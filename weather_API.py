a9e4f6a046e84e497863ac5ad857dfdc848f7bf4
Diego Roses <diroq44@gmail.com>
2025-09-08 16:11:23 -0600

    Add display error, NEXT DISPLAY WEATHER

--- weather_API.py
+++ weather_API.py
@@ -9,10 +9,10 @@ class WeatherApp(QWidget):
         super().__init__()
         self.city_label = QLabel("Enter city name:", self)
         self.city_input_label = QLineEdit(self)
-        self.get_weather_button = QPushButton("Get weather", self)
-        self.temp_label = QLabel("70 °C", self)
-        self.emoji_label = QLabel("🔆", self)
-        self.description_weather_label = QLabel("Sunny", self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temp_label = QLabel(self)
+        self.emoji_label = QLabel(self)
+        self.description_weather_label = QLabel(self)
         self.initUI()
 
     def initUI(self):
@@ -57,7 +57,7 @@ class WeatherApp(QWidget):
             }
             QPushButton#get_weather_button{
                 font-size: 30px;
-                font-syle: bold;
+                font-style: bold;
             }           
             QLabel#temp_label{
                 font-size: 75px;               
@@ -70,7 +70,62 @@ class WeatherApp(QWidget):
                 font-size: 50px;               
             }
                             """)
-    
+        
+        self.get_weather_button.clicked.connect(self.get_weather)
+
+#Connecting to the API
+    def get_weather(self):
+        api_key = "c8adbf13b8b1778b7906b4a1e1cfadde"
+        city = self.city_input_label.text()
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            if data["cod"] == 200:
+                self.display_weather(data)
+
+#Here we create messages in case a bad or lost connection with the API
+        except requests.exceptions.HTTPError as http_error:
+            match response.status_code:
+                case 400:
+                    self.display_error("Bad request: \n Please check your input")
+                case 401:
+                    self.display_error("Unauthorized: \n Invalid API")
+                case 403:
+                    self.display_error("Access denied")
+                case 404:
+                    self.display_error("Not found: \nCity not found")
+                case 500:
+                    self.display_error("Server error: \nTry later")
+                case 502:
+                    self.display_error("Bad getaway: \nInvalid response from server")
+                case 503:
+                    self.display_error("Service unavailable: \nServer is down")
+                case 504:
+                    self.display_error("Getaway timeout: \nNo response from server")
+                case _:
+                    self.display_error("HTTP error occured")
+
+        except requests.exceptions.ConnectionError:
+            self.display_error("Connection error:\nCheck your internet connection")
+        except requests.exceptions.Timeout:
+            self.display_error("Timeout error:\nThe request timeout")
+        except requests.exceptions.TooManyRedirects:
+            self.display_error("Too Many Redirects error:\nCheck the URL")
+        except requests.exceptions.RequestException as req_error:
+            self.display_error(f"Request error:\n{req_error}")
+
+    def display_error(self, message):
+#We just take the error messages from the get weather fuction and display it on the app
+        self.temp_label.setStyleSheet("font-size: 30px;")
+        self.temp_label.setText(message)
+
+    def display_weather(self, data):
+        print(data)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     weather_app = WeatherApp()
